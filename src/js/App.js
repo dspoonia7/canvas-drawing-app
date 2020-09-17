cafa1792df5d78f561c51f54e18fe35ef2bfd714
Dharmendra Poonia <dspoonia7@gmail.com>
2020-09-17 22:33:02 +0530

    React project setup

---
+++ src/js/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MainWrapper from './MainWrapper';
+
+function App() {
+  return (
+    <div className='app'>
+      <header className='app-header'>
+        <h1>Canvas drawing board app</h1>
+      </header>
+      <MainWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/js/MainWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContainer() {
+  return (
+    <div className='main-wrapper'>
+      <h5>Content</h5>
+    </div>
+  );
+}
+
+export default MainContainer;
