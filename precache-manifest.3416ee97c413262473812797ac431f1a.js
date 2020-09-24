97f88dbd03305ee40b0526d6af9aa8228933ec64
Dharmendra Poonia <dspoonia7@gmail.com>
2020-09-24 08:41:52 +0530

    Updates

---
+++ precache-manifest.3416ee97c413262473812797ac431f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590ecf919f8707d1bcc29b8e00d2b3b0",
+    "url": "/canvas-drawing-app/index.html"
+  },
+  {
+    "revision": "8d231fae007e6e77156f",
+    "url": "/canvas-drawing-app/static/css/main.13dde9c4.chunk.css"
+  },
+  {
+    "revision": "77ba5967b0c0536bf225",
+    "url": "/canvas-drawing-app/static/js/2.448c7fdb.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/canvas-drawing-app/static/js/2.448c7fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d231fae007e6e77156f",
+    "url": "/canvas-drawing-app/static/js/main.a2e9982a.chunk.js"
+  },
+  {
+    "revision": "89a4944a7bb1fa80616d",
+    "url": "/canvas-drawing-app/static/js/runtime-main.77f5eee3.js"
+  }
+]);
